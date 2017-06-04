fbbdeda2218fba73930b1afd37d5396228710e34
Ziady Mubaraq <ziterz@hotmail.com>
2017-06-04 13:31:30 +0700

    Initial commit

---
+++ app/src/main/java/com/ziterz/navigationdrawer/ImportFragment.java
@@ -0,0 +1,29 @@
+package com.ziterz.navigationdrawer;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ImportFragment extends Fragment {
+
+
+    public ImportFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_import, container, false);
+    }
+
+}
